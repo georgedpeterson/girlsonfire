2e811640e08fe21ff5a2e89df12626d889fdb820
georgedpeterson <george.d.peterson@gmail.com>
2015-02-07 16:39:25 -0500

    Added moveBase & other functions in autnouus

--- Tank Drive/src/org/usfirst/frc/team5679/robot/Robot.java
+++ Tank Drive/src/org/usfirst/frc/team5679/robot/Robot.java
@@ -9,10 +9,12 @@ import edu.wpi.first.wpilibj.CANTalon;
 import edu.wpi.first.wpilibj.CounterBase.EncodingType;
 import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj.Encoder;
+import edu.wpi.first.wpilibj.Gyro;
 import edu.wpi.first.wpilibj.IterativeRobot;
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.Joystick.ButtonType;
 import edu.wpi.first.wpilibj.Relay;
+import edu.wpi.first.wpilibj.RobotDrive;
 import edu.wpi.first.wpilibj.Talon;
 import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.interfaces.Accelerometer;
@@ -35,13 +37,19 @@ public class Robot extends IterativeRobot {
 	CANTalon uppieDownie0=new CANTalon(2);
 	CANTalon uppieDownie1=new CANTalon(3);
 	Joystick wibblyWobblyDrive=new Joystick(0);
-	//TODO: carriage joystick id
+	
 	Joystick wibblyWobblyCarriage=new Joystick(1);
+	
+	RobotDrive drive = new RobotDrive(leftie0, leftie1, rightie0, rightie1);
 	
 	DigitalInput limitSwitchTop = new DigitalInput(4);
 	DigitalInput limitSwitchBottom = new DigitalInput(5);
 	
 	Relay clawRelay = new Relay(0);
+	
+	Gyro gyro = new Gyro(0);
+	double startingAngle = 0;
+	static final double Kp = 1.0;
 	
 	BuiltInAccelerometer accel = new BuiltInAccelerometer();
 	
@@ -68,7 +76,7 @@ public class Robot extends IterativeRobot {
 		SmartDashboard.putString("robot init", "robot init");
 		
 		rightEncoder.reset();
-		leftEncoder.reset();		
+		leftEncoder.reset();	
     }
     
     /**
@@ -77,72 +85,90 @@ public class Robot extends IterativeRobot {
     public void autonomousinit() {
 
 		SmartDashboard.putString("autonomous init", "autonomous init");
+		gyro.reset();
 	}
 
     /**
      * This function is called periodically during autonomous control
      */
     public void autonomousPeriodic() {
-
-    	try {
-			Thread.sleep(1000);
-		} catch (InterruptedException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-    	
-    	if(runOnce)
-    	{
-    		runOnce = false;
-        	leftie0.set(.4);
-            leftie1.set(.4);
-            rightie0.set(-.4);
-            rightie1.set(-.4);	
-    	}   
-    	
-    	if (!reverse && (rightEncoder.getDistance() >= 1.5 || leftEncoder.getDistance() >=1.5)) {  
-            leftie0.set(.2);
-            leftie1.set(.2);
-            rightie0.set(-.2);
-            rightie1.set(-.2); 
-    	}
-    	
-    	if (!reverse && (rightEncoder.getDistance() >= 3 || leftEncoder.getDistance() >=3)) {  
-            leftie0.set(-.4);
-            leftie1.set(-.4);
-            rightie0.set(.4);
-            rightie1.set(.4);             
-
-            rightEncoder.reset();
-            leftEncoder.reset();
-            
-            rightEncoder.setReverseDirection(true);
-            leftEncoder.setReverseDirection(true);
-            
-            reverse = true;
-        }
-    	
-    	if (reverse && (rightEncoder.getDistance() >= 1.5 || leftEncoder.getDistance() >=1.5)) {
-    		leftie0.set(-.2);
-            leftie1.set(-.2);
-            rightie0.set(.2);
-            rightie1.set(.2); 
-    	}
-    	
-    	if (reverse && (rightEncoder.getDistance() >= 3 || leftEncoder.getDistance() >=3)) {
-    		leftie0.set(0);
-            leftie1.set(0);
-            rightie0.set(0);
-            rightie1.set(0); 
-            
-            reverse = false;
-    	}                  
-    	
-    	     
-
-        SmartDashboard.putNumber("Right Encoder", rightEncoder.getDistance());
-        SmartDashboard.putNumber("Left Encoder",-1 * leftEncoder.getDistance());
+		double angle = gyro.getAngle();
+		moveBase(2, .2, angle);
+//		if(angle - startingAngle <= 359) {
+//			moveBase(0, .1, 359);
+//		} else {
+//			moveBase(0, 0, 0);
+//		}   
+//		controlClaw(true);
+//		controlClaw(false);
+//		rightEncoder.reset();
+//		leftEncoder.reset();
+//		rightEncoder.setReverseDirection(true);
+//		leftEncoder.setReverseDirection(true);
+//		moveBase(2, .2, angle);
 		
+//    	if(runOnce)
+//    	{
+//    		runOnce = false;
+//    		startingAngle = angle;	
+//    	}       	
+//    	
+//    		
+//		if(angle - startingAngle <= 90) {
+//			drive.drive(.1, 90 * Kp); 
+//		} else {
+//    		drive.drive(0, angle * Kp);
+//		}   		                 
+//        
+    	SmartDashboard.putNumber("Angle", angle);
+        SmartDashboard.putNumber("Right Encoder", rightEncoder.getDistance());
+        SmartDashboard.putNumber("Left Encoder", leftEncoder.getDistance());
+    }
+    
+    /**
+     * This function is for moving forward a set number of feet.
+     */
+    public void moveBase (double feet, double speed, double angle){
+    	if(rightEncoder.getDistance() >= feet || leftEncoder.getDistance() >= feet){
+    		drive.drive(0, 0);
+    	}else{
+    		drive.drive(speed, angle * Kp); 
+    	}   
+	}
+    
+    /**
+     * This function is for moving the carriage.
+     */
+    public void moveCarriage (double speed){
+    	boolean moveValid = true;
+        if(speed > 0 && !limitSwitchTop.get())
+        {
+        	moveValid = false;
+        }else if (speed < 0 && !limitSwitchBottom.get())
+        {
+        	moveValid = false;
+        }
+        
+        if(moveValid)
+        {
+        	uppieDownie0.set(speed);
+        	uppieDownie1.set(speed);
+        }else
+        {
+        	uppieDownie0.set(0);  
+            uppieDownie1.set(0); 
+        }
+    }
+    
+    /**
+     * This function is for opening and closing the claw.
+     */ 
+    public void controlClaw (boolean open){
+        if (open) {
+        	clawRelay.set(Relay.Value.kForward); 
+        } else {
+        	clawRelay.set(Relay.Value.kOff);
+        }
     }
     
     /**
@@ -150,27 +176,29 @@ public class Robot extends IterativeRobot {
      */
     public void teleopPeriodic() {
         double LP=-wibblyWobblyDrive.getRawAxis(1);
-        double RP=wibblyWobblyDrive.getRawAxis(5);
+        double RP=-wibblyWobblyDrive.getRawAxis(5);
         double UD = wibblyWobblyCarriage.getRawAxis(1);
         boolean clawButton = wibblyWobblyCarriage.getRawButton(1);
         boolean moveValid = true;
-        
-        if(Math.abs(LP)<.1){
-        	LP=0;
-        	
-	        if(Math.abs(RP)<.1){
-	        	RP=0;
-	        }
-        }
-        
-        if(Math.abs(UD)<.1){
-        	UD=0;
-        }
-        
-        leftie0.set(LP);
-        leftie1.set(LP);
-        rightie0.set(RP);
-        rightie1.set(RP); 
+
+		drive.tankDrive(LP, RP);
+//	        
+//	        if(Math.abs(LP)<.1){
+//	        	LP=0;
+//	        	
+//		        if(Math.abs(RP)<.1){
+//		        	RP=0;
+//		        }
+//	        }
+//	        
+//	        if(Math.abs(UD)<.1){
+//	        	UD=0;
+//	        }
+//	        
+//	        leftie0.set(LP);
+//	        leftie1.set(LP);
+//	        rightie0.set(RP);
+//	        rightie1.set(RP); 
         
         if(UD > 0 && !limitSwitchTop.get())
         {
@@ -204,8 +232,6 @@ public class Robot extends IterativeRobot {
         SmartDashboard.putBoolean("Upper Limit", limitSwitchTop.get());
         SmartDashboard.putBoolean("Lower Limit", limitSwitchBottom.get());
         SmartDashboard.putBoolean("Move Valid", moveValid);
-        SmartDashboard.putNumber("Operator Left", LP);
-        SmartDashboard.putNumber("Operator Right", RP);
         SmartDashboard.putNumber("Operator UpDown", UD);
         
         SmartDashboard.putNumber("Right Encoder", rightEncoder.getDistance());
